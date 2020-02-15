c01cc8ce195beace87b5cebe98d9f4eb5954bd61
asosunoff <al.sosunoff@gmail.com>
2020-02-15 13:35:58 +0300

    task 2 in 2 module is resolved

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,6 +10,15 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
+  if (!name) {
+    return false;
+  }
+
+  let withoutSpace = '[^\\s]';
+  let minFourSign = '{4,}';
+  let regex = new RegExp(`^${withoutSpace}${minFourSign}$`);
+
+  return regex.test(name);
 }
 
 function sayHello() {
@@ -21,3 +30,5 @@ function sayHello() {
     print('Некорректное имя');
   }
 }
+
+console.log(isValid('Ilia Burlak'));
