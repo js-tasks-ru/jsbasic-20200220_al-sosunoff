c299ea73ac901204428ce6920bbe11551238cefb
asosunoff <al.sosunoff@gmail.com>
2020-02-15 14:02:47 +0300

    task 5 in module 2 is resolved

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,10 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
+  if (str.length <= maxlength) {
+    return str;
+  }
 
+  let strCrop = str.substr(0, maxlength - 1);
+  return `${strCrop}…`;
 }
