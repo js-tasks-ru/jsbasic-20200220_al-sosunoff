e3fd55970da60eaab97fccdee0839a155cd51bb6
asosunoff <al.sosunoff@gmail.com>
2020-02-23 14:41:12 +0300

    task 2 from module 3 is completed

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,22 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  let numberArray = toNumberArray(str);
+
+  return {
+    min: Math.min(...numberArray),
+    max: Math.max(...numberArray),
+  };
+}
+
+/**
+ * toNumberArray
+ * @param {string} str 
+ * @returns {Array} массив с числами
+ */
+function toNumberArray (str) {
+  return str
+    .split(/[^\d.-]/)
+    .filter(e => e)
+    .map(e => Number(e));
 }
