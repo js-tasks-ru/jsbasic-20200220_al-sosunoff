5e06153a60ee0f0a4d0e5571bb8257e8939e9150
asosunoff <al.sosunoff@gmail.com>
2020-02-23 15:09:35 +0300

    task 3 from module 3 is completed

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,4 +3,13 @@
  * @returns {string}
  */
 function camelize(str) {
+  let wordArray = str.split(/-/);
+  let wordUnchanged = wordArray[0];
+  let wordArrayByChanged = wordArray.slice(1);
+
+  return wordUnchanged + wordArrayByChanged.map(upperFirstCharacterInWord).join('');
+}
+
+function upperFirstCharacterInWord (word) {
+  return word[0].toUpperCase() + word.slice(1);
 }
