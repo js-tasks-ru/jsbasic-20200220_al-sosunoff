6be2afa3bf4b6f57c4cd1a4829db8e5023d3f1aa
asosunoff <al.sosunoff@gmail.com>
2020-02-24 23:28:27 +0300

    tasks 1, 2 from module 4 is completed

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,21 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  let ul = createElement('ul');
+
+  friends.forEach(friend => {
+    ul.appendChild(createElement('li', `${friend.firstName} ${friend.lastName}`));
+  });
+
+  return ul;
+}
+
+function createElement(tagName, innerText) {
+  let li = document.createElement(tagName);
+  
+  if (innerText) {
+    li.innerText = innerText;
+  }
+
+  return li;
 }
