8950f312c3c794d19e5aaefdf021316465037a91
asosunoff <al.sosunoff@gmail.com>
2020-02-26 23:59:13 +0300

    task 1 from module 5 is completed

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -3,4 +3,37 @@
  * @param {Element} table
  */
 function highlight(table) {
+  let rows = Array.from(table.rows);
+
+  for (const row of rows) {
+    let available = row.querySelector("td[data-available]");
+
+    if (available) {
+      switch (available.dataset.available) {
+      case "true":
+        row.classList.add("available");
+        break;
+      case "false":
+        row.classList.add("unavailable");
+        break;
+      }
+    }
+
+    if (!row.getAttribute('hidden')) {
+      row.setAttribute('hidden', true);
+    }
+    
+    switch (row.cells[2].innerText) {
+    case "m":
+      row.classList.add('male');
+      break;
+    case "f":
+      row.classList.add('female');
+      break;
+    }
+
+    if (+row.cells[1].innerText < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  }
 }
